42288c9460adaa87c064e3690002d3379159f1f4
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-27 07:32:43 -0700

    background color changes with params

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Main() {
+  const params = useParams();
+  console.log('params', params);
   return (
-    <div>Main</div>
+    <div style={{ backgroundColor: `rgb(${params.red}, ${params.green}, ${params.blue})` }}>
+      PICK A COLOR
+    </div>
   );
 }
